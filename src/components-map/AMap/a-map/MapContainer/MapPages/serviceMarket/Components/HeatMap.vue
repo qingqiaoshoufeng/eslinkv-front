12271a5b3cfd5af320d3ee37ba7b5a6d3a9c9fa0
於志强 <zhiqiang.yu@eslink.com>
2020-11-16 20:14:45 +0800

    提交

--- src/components-map/AMap/a-map/MapContainer/MapPages/serviceMarket/Components/HeatMap.vue
+++ src/components-map/AMap/a-map/MapContainer/MapPages/serviceMarket/Components/HeatMap.vue
@@ -1,22 +1,20 @@
 <script>
 import heatMapData from '@/assets/amap/json/heatMap.json';
-// import overlayMixin from '../../mixins/overlayMixin.js';
+import overlayMixin from '../../mixins/overlayMixin.js';
 export default {
-	// mixins: [overlayMixin],
-	created() {
-		this.initHeatMap();
-	},
+	mixins: [overlayMixin],
 	methods: {
-		initHeatMap() {
-			let map = this.$parent.map;
-			this.instance = new AMap.HeatMap(map, {
+		init() {
+			// let map = this.$parent.map;
+			let instance = new AMap.HeatMap(this.$amap, {
 				radius: 80, //给定半径
 				opacity: [0, 0.8],
 			});
-			this.instance.setDataSet({
+			instance.setDataSet({
 				data: heatMapData,
 				max: 4000,
 			});
+			this.instanceArr.push(instance);
 		},
 	},
 	render() {
