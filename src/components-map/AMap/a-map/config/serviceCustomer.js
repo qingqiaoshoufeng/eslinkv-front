ddf42546a950b884b03e1e4ac042881ba31c885e
於志强 <zhiqiang.yu@eslink.com>
2020-11-19 11:30:49 +0800

    xiuzheng

--- src/components-map/AMap/a-map/config/serviceCustomer.js
+++ src/components-map/AMap/a-map/config/serviceCustomer.js
@@ -1,6 +1,5 @@
 //legend配置
 export const SERVICELEGENDCUSTOMERMAP = {
-<<<<<<< HEAD
     Grouphall: {
         label: '集团大厅',
         legendIcon: 'icontulijituandating',
@@ -25,62 +24,36 @@ export const SERVICELEGENDCUSTOMERMAP = {
         icon: 'iconsansheliandong',
         component: 'ThreeSocialLinkage',
     }
-=======
-	Grouphall: {
-		label: '集团大厅',
-		legendIcon: 'icontulijituandating',
-		icon: 'iconjituandating',
-		component: 'Grouphall',
-	},
-	BranchCompany: {
-		label: '分公司',
-		legendIcon: 'icontulifengongsi',
-		icon: 'iconfengongsi',
-		component: 'BranchCompany',
-	},
-	ServiceNetworkStation: {
-		label: '服务网点',
-		legendIcon: 'icontulifuwuwangdian',
-		icon: 'iconfuwuwangdian',
-		component: 'ServiceNetworkStation',
-	},
-	ThreeSocialLinkage: {
-		label: '三社联动',
-		legendIcon: 'icontulisansheliandong',
-		icon: 'iconsansheliandong',
-		component: 'ThreeSocialLinkage',
-	},
->>>>>>> 88e162c5157e20a6a53d77f25916dd0bd0cec80b
 }
 
 export const SERVICELEGEND_CUSTOMER = {
-	Grouphall: true,
-	BranchCompany: true,
-	ServiceNetworkStation: true,
-	ThreeSocialLinkage: true,
+    Grouphall: true,
+    BranchCompany: true,
+    ServiceNetworkStation: true,
+    ThreeSocialLinkage: true,
 }
 
 //覆盖物详情
 export const OVERLAYINFOMAP_SERVICE_CUSTOMER = {
-	BranchCompany: {
-		fields: {
-			name: {},
-		},
-	},
-	ServiceNetworkStation: {
-		fields: {
-			name: {},
-		},
-	},
-	ThreeSocialLinkage: {
-		fields: {
-			name: {},
-		},
-		isShowMore: true,
-	},
-	Grouphall: {
-		fields: {
-			name: {},
-		},
-	},
+    BranchCompany: {
+        fields: {
+            name: {},
+        },
+    },
+    ServiceNetworkStation: {
+        fields: {
+            name: {},
+        },
+    },
+    ThreeSocialLinkage: {
+        fields: {
+            name: {},
+        },
+        isShowMore: true,
+    },
+    Grouphall: {
+        fields: {
+            name: {},
+        },
+    },
 }
--- src/components-map/AMap/a-map/config/serviceICcustomer.js
+++ src/components-map/AMap/a-map/config/serviceICcustomer.js
@@ -1,6 +1,5 @@
 //legend配置
 export const SERVICELEGENDICCUSTOMERMAP = {
-<<<<<<< HEAD
     ICcustomer: {
         label: '工商户',
         legendIcon: 'icontuli-gongshanghu',
@@ -8,26 +7,18 @@ export const SERVICELEGENDICCUSTOMERMAP = {
         component: 'ICcustomer',
     }
 
-=======
-	ICcustomer: {
-		label: '工商户',
-		legendIcon: 'icontulijituandating',
-		icon: 'iconjituandating',
-		component: 'ICcustomer',
-	},
->>>>>>> 88e162c5157e20a6a53d77f25916dd0bd0cec80b
 }
 
 export const SERVICELEGEND_ICCUSTOMER = {
-	ICcustomer: true,
+    ICcustomer: true,
 }
 
 //覆盖物详情
 export const OVERLAYINFOMAP_ICCUSTOMER = {
-	ICcustomer: {
-		fields: {
-			name: {},
+    ICcustomer: {
+        fields: {
+            name: {},
         },
-        isShowMore:false
-	},
+        isShowMore: false
+    },
 }
