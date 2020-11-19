90d6a140efb7e8ab1e65bf7130bf78517b426b88
zhepei.shen <zhepei.shen@eslink.com>
2020-11-19 10:17:15 +0800

    柱形图优化

--- src/business-components/Serves/s-bar3/options.js
+++ src/business-components/Serves/s-bar3/options.js
@@ -85,7 +85,7 @@ export default function (data, config) {
 			showBackground: true,
 			itemStyle: {
 				normal: {
-					color: config.color2,
+					color: config.color1,
 				}
 			},
 			barWidth: 10,
@@ -97,7 +97,7 @@ export default function (data, config) {
 			showBackground: true,
 			itemStyle: {
 				normal: {
-					color: config.color1,
+					color: config.color2,
 				}
 			},
 			data: data.yValue
