b23e4a8176c5c0d0bd363b78c84ceeb28b1ec6b5
xjm <xujm@citycloud,com.cn>
2020-11-23 10:38:54 +0800

    feat: 首页数据

--- src/business-components/Home/h-linshi2/index.vue
+++ src/business-components/Home/h-linshi2/index.vue
@@ -1,33 +1,34 @@
 <template>
-	<div class="widget-part" :style="styles">
+	<div class="widget-part" :style="styles" ref="img">
     <section class="title">
       <div class="title-txt">杭燃优家</div>
       <div class="color"></div>
     </section>
     <section class="detail">
       <div class="detail-item">
-        <div class="detail-item-icon"></div>
+        <div class="detail-item-icon" :style="`background-image: url(${
+				currBuilding.icon1
+			})`"></div>
         <div>
-          <div class="detail-item-title">年度销量</div>
-          <div class="detail-item-num">4856 <span>台</span></div>
+          <div class="detail-item-title">{{ currBuilding.desc1 }}</div>
+          <div class="detail-item-num">{{ currBuilding.value1 }} <span>{{ currBuilding.unit1 }}</span></div>
         </div>
       </div>
       <div class="detail-item">
-        <div class="detail-item-icon"></div>
+        <div class="detail-item-icon money"></div>
         <div>
-          <div class="detail-item-title">年度销售金额</div>
-          <div class="detail-item-num">326,084 <span>元</span></div>
+          <div class="detail-item-title">{{ currBuilding.desc2 }}</div>
+          <div class="detail-item-num">{{ currBuilding.value2 | toThousand }} <span>元</span></div>
         </div>
       </div>
     </section>
     <section class="building">
       <div class="circle">
-        <div class="item" :class="[`index-${index1}`]">燃气报警系统</div>
-        <div class="item" :class="[`index-${index2}`]">燃气保险</div>
-        <div class="item" :class="[`index-${index3}`]">金属管</div>
-        <div class="item" :class="[`index-${index4}`]">非居民燃气设施</div>
-        <div class="item" :class="[`index-${index5}`]">供暖工程</div>
-        <div class="item" :class="[`index-${index6}`]">居民燃气设施</div>
+        <div
+            class="item"
+            :class="[getItemIndex(i)]"
+            v-for="(k, i) in data.building"
+        >{{k.name}}</div>
       </div>
     </section>
     <section class="title">
@@ -37,11 +38,11 @@
     <section class="fans">
       <div style="text-align: left">
         <div class="fan-title">数媒粉丝日活(人)</div>
-        <div class="fan-num font-num">265</div>
+        <div class="fan-num font-num">{{ data.fanDailyNum }}</div>
       </div>
       <div style="text-align: right">
         <div class="fan-title">数媒粉丝总数(人)</div>
-        <div class="fan-num font-num">265,455</div>
+        <div class="fan-num font-num">{{ data.fanTotalNum }}</div>
       </div>
     </section>
 	</div>
@@ -67,11 +68,13 @@
 	}
 	const value = {
 		config: {
-			transform: 149,
+			transform: -30,
 			left: -50,
 		},
     api: {
 		  data: {
+		    fanDailyNum: 123,
+		    fanTotalNum: 425673,
 		    building: [
           {
             name: '燃气报警系统',
@@ -142,6 +145,7 @@
     data () {
 		  return {
         buildingTimer: null,
+        loopIndex: 1,
         index1: 1,
         index2: 2,
         index3: 3,
@@ -150,7 +154,16 @@
         index6: 6
       }
     },
+    computed: {
+		  currBuilding () {
+		    return this.data?.building[((this.loopIndex - 1) % 6)] || {}
+      }
+    },
     methods: {
+		  getItemIndex (i) {
+		    const n = (i + this.loopIndex) % 6
+		    return `index-${n}`
+      },
 		  count (n) {
 		    if (n === 6) {
 		      n = 1
@@ -164,14 +177,15 @@
 			this.configSource = this.parseConfigSource(config, configSource)
 			this.configValue = this.parseConfigValue(config, value)
       this.buildingTimer = setInterval(() => {
-        this.index1 = this.count(this.index1)
-        this.index2 = this.count(this.index2)
-        this.index3 = this.count(this.index3)
-        this.index4 = this.count(this.index4)
-        this.index5 = this.count(this.index5)
-        this.index6 = this.count(this.index6)
+        this.loopIndex++
       }, 3000)
 		},
+    mounted() {
+      setTimeout(()=>{
+        this.$refs.img.style.transform=`perspective(763px) rotateY(${this.config.config.transform}deg)`
+        this.$refs.img.style.right=`${this.config.config.left}px`
+      },1500)
+    },
     beforeDestroy() {
 		  clearInterval(this.buildingTimer)
       this.buildingTimer = null
@@ -179,6 +193,9 @@
   }
 </script>
 <style lang="scss" scoped>
+.widget-part {
+  transition: all .5s;
+}
 .title {
   display: flex;
   align-items: center;
@@ -227,6 +244,9 @@
       height: 80px;
       margin-right: 8px;
       background: #0f3b69;
+      &.money {
+        background-image: url("/static/images/home/home-u-0.svg");
+      }
     }
     .detail-item-title {
       font-size: 24px;
