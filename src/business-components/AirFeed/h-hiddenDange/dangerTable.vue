03a5e06914d4e9ea942602da704cdc49485c1fa3
zhepei.shen <zhepei.shen@eslink.com>
2020-11-27 17:46:19 +0800

    首页调整

--- src/business-components/AirFeed/h-hiddenDange/dangerTable.vue
+++ src/business-components/AirFeed/h-hiddenDange/dangerTable.vue
@@ -1,147 +1,174 @@
 <template>
-  <div class="table">
-    <ul class="title">
-      <li></li>
-      <li>隐患排行</li>
-      <li>设备类型</li>
-      <li>数量(件)</li>
-      <li>已处理(件)</li>
-      <li>未处理(件)</li>
-    </ul>
-    <div class="content" v-if="data">
-      <vue-seamless-scroll
-          :data="data || []"
-          class="content-warp"
-          :class-option="classOption"
-      >
-        <ul
-            class="content-row"
-            v-for="(item, index) in data"
-            :key="index"
-        >
-          <li class="font-num">0{{ index + 1 }}</li>
-          <li>{{ item.name }}</li>
-          <li>{{ item.type }}</li>
-          <li>{{ item.total }}</li>
-          <li>{{ item.handled }}</li>
-          <li>{{ item.unhandled }}</li>
-        </ul>
-      </vue-seamless-scroll>
-    </div>
-  </div>
+	<div class="table">
+		<ul class="title">
+			<li></li>
+			<li>隐患排行</li>
+			<li>设备类型</li>
+			<li>数量(件)</li>
+			<li>已处理(件)</li>
+			<li>未处理(件)</li>
+		</ul>
+		<div class="content" v-if="data">
+			<vue-seamless-scroll
+				:data="data || []"
+				class="content-warp"
+				:class-option="classOption"
+			>
+				<ul
+					class="content-row"
+					v-for="(item, index) in data"
+					:key="index"
+				>
+					<li class="font-num">0{{ index + 1 }}</li>
+					<li>{{ item.name }}</li>
+					<li>{{ item.type }}</li>
+					<li>{{ item.total }}</li>
+					<li>{{ item.handled }}</li>
+					<li>{{ item.unhandled }}</li>
+				</ul>
+			</vue-seamless-scroll>
+		</div>
+	</div>
 </template>
 <script>
-import VueSeamLess from 'vue-seamless-scroll';
+	import VueSeamLess from 'vue-seamless-scroll';
 
-export default {
-  components: {
-    VueSeamLess,
-  },
-  props: ['data'],
-  computed: {
-    classOption() {
-      return {
-        step: 0.2, // 数值越大速度滚动越快
-        limitMoveNum: this.data?.length, // 开始无缝滚动的数据量
-        hoverStop: true, // 是否开启鼠标悬停stop
-        direction: 1, // 0向下 1向上 2向左 3向右
-        openWatch: true, // 开启数据实时监控刷新dom
-        singleHeight: 0, // 单步运动停止的高度(默认值0是无缝不停止的滚动) direction => 0/1
-        singleWidth: 0, // 单步运动停止的宽度(默认值0是无缝不停止的滚动) direction => 2/3
-        waitTime: 1000, // 单步运动停止的时间(默认值1000ms)
-      };
-    },
-  }
-};
+	export default {
+		components: {
+			VueSeamLess,
+		},
+		props: ['data'],
+		computed: {
+			classOption() {
+				return {
+					step: 0.2, // 数值越大速度滚动越快
+					limitMoveNum: this.data?.length, // 开始无缝滚动的数据量
+					hoverStop: true, // 是否开启鼠标悬停stop
+					direction: 1, // 0向下 1向上 2向左 3向右
+					openWatch: true, // 开启数据实时监控刷新dom
+					singleHeight: 0, // 单步运动停止的高度(默认值0是无缝不停止的滚动) direction => 0/1
+					singleWidth: 0, // 单步运动停止的宽度(默认值0是无缝不停止的滚动) direction => 2/3
+					waitTime: 1000, // 单步运动停止的时间(默认值1000ms)
+				};
+			},
+		}
+	};
 </script>
 <style lang="scss" scoped>
-.table {
-  flex: none;
-  width: 100%;
-  .title {
-    width: 100%;
-    height: 32px;
-    background: rgba(23, 123, 255, 0.2);
-    display: flex;
-    align-items: center;
-    box-sizing: border-box;
-    & > li {
-      font-size: 18px;
-      color: #00CBF4;
-      text-align: left;
-      margin-right: 24px;
-      flex: none;
-      &:nth-child(1) {
-        width: 24px;
-        margin-right: 8px;
-      }
-      &:nth-child(2) {
-        width: 128px;
-      }
-      &:nth-child(3) {
-        width: 128px;
-      }
-      &:nth-child(4) {
-        width: 84px;
-      }
-      &:nth-child(5) {
-        width: 84px;
-      }
-      &:nth-child(6) {
-        width: 84px;
-      }
-    }
-  }
-  .content {
-    height: 155px;
+	.table {
+		flex: none;
+		width: 100%;
 
-    .content-warp {
-      height: 155px;
-      overflow: hidden;
-    }
-    .content-row {
-      height: 24px;
-      display: flex;
-      align-items: center;
-      box-sizing: border-box;
-      margin-top: 8px;
-      &:nth-child(1)>li:nth-child(1){background: #FF7217;}
-      &:nth-child(2)>li:nth-child(1){background: #0057A9;}
-      &:nth-child(3)>li:nth-child(1){background: #0057A9;}
-      & > li {
-        font-size: 18px;
-        color: #fff;
-        text-align: left;
-        margin-right: 24px;
-        flex: none;
-        &:nth-child(1) {
-          width: 24px;
-          height: 24px;
-          text-align: center;
-          line-height: 24px;
-          background: rgba(255, 255, 255, 0.2);
-          margin-right: 8px;
-        }
-        &:nth-child(2) {
-          width: 128px;
-        }
-        &:nth-child(3) {
-          width: 128px;
-        }
-        &:nth-child(4) {
-          width: 84px;
-        }
-        &:nth-child(5) {
-          width: 84px;
-          color: #00FFCF;
-        }
-        &:nth-child(6) {
-          width: 84px;
-          color: #FF7217;
-        }
-      }
-    }
-  }
-}
+		.title {
+			width: 100%;
+			height: 32px;
+			background: rgba(23, 123, 255, 0.2);
+			display: flex;
+			align-items: center;
+			box-sizing: border-box;
+			white-space: nowrap;
+
+			& > li {
+				font-size: 18px;
+				color: #00CBF4;
+				text-align: left;
+				margin-right: 24px;
+				flex: none;
+
+				&:nth-child(1) {
+					width: 24px;
+					margin-right: 8px;
+				}
+
+				&:nth-child(2) {
+					width: 128px;
+				}
+
+				&:nth-child(3) {
+					width: 128px;
+				}
+
+				&:nth-child(4) {
+					width: 84px;
+				}
+
+				&:nth-child(5) {
+					width: 84px;
+				}
+
+				&:nth-child(6) {
+					width: 84px;
+				}
+			}
+		}
+
+		.content {
+			height: 155px;
+
+			.content-warp {
+				height: 155px;
+				overflow: hidden;
+			}
+
+			.content-row {
+				height: 24px;
+				display: flex;
+				align-items: center;
+				box-sizing: border-box;
+				margin-top: 8px;
+
+				&:nth-child(1) > li:nth-child(1) {
+					background: #FF7217;
+				}
+
+				&:nth-child(2) > li:nth-child(1) {
+					background: #0057A9;
+				}
+
+				&:nth-child(3) > li:nth-child(1) {
+					background: #0057A9;
+				}
+
+				& > li {
+					font-size: 18px;
+					color: #fff;
+					text-align: left;
+					margin-right: 24px;
+					flex: none;
+
+					&:nth-child(1) {
+						width: 24px;
+						height: 24px;
+						text-align: center;
+						line-height: 24px;
+						background: rgba(255, 255, 255, 0.2);
+						margin-right: 8px;
+					}
+
+					&:nth-child(2) {
+						width: 128px;
+					}
+
+					&:nth-child(3) {
+						width: 128px;
+					}
+
+					&:nth-child(4) {
+						width: 84px;
+					}
+
+					&:nth-child(5) {
+						width: 84px;
+						color: #00FFCF;
+					}
+
+					&:nth-child(6) {
+						width: 84px;
+						color: #FF7217;
+					}
+				}
+			}
+		}
+	}
 </style>
 
--- src/business-components/Home/h-left/pie.vue
+++ src/business-components/Home/h-left/pie.vue
@@ -1,104 +1,109 @@
 <template>
-  <div class="wrap">
-    <div class="chart" ref="chart"></div>
-    <div class="unit">单位：万m³</div>
-  </div>
+	<div class="wrap">
+		<div class="chart" ref="chart"></div>
+		<div class="unit">单位：万m³</div>
+	</div>
 </template>
 
 <script>
 
-export default {
-  props: ['data'],
-  watch: {
-    data: {
-      handler(val) {
-        this.$nextTick(() => {
-          this.instance = echarts.init(this.$refs.chart).setOption(this.getOption())
-        })
-      },
-      deep: true,
-      immediate: true,
-    },
-  },
-  methods: {
-    getOption () {
-      const total = this.data.reduce((p, n) => p + n.value, 0)
-      return {
-        title: {
-          text: '区域',
-          subtext: '销售气量',
-          left: 'center',
-          top: '42%',
-          textStyle: {
-            color: '#fff',
-            fontSize: 24,
-            fontWeight: 600
-          },
-          subtextStyle: {
-            color: '#fff',
-            fontSize: 24,
-            fontWeight: 600
-          }
-        },
-        series: [{
-          type: 'pie',
-          center: ['50%', '50%'],
-          radius: ['52%', '64%'],
-          avoidLabelOverlap: false,
-          startAngle: 270,
-          color: ['#db4f49', '#367cf6', '#84e4fc', '#f8cd47', '#fceb50', '#ed9144', '#5fcbb9'],
-          label: {
-            formatter: function(params) {
-              const percent = (params.value * 100 / total).toFixed(2)
-              return `{normal|${params.name}}\n{value|${params.value} ${percent}%}`
-            },
-            padding: [0, -100],
-            rich: {
-              normal: {
-                fontSize: 20,
-                lineHeight: 24,
-                color: '#fff',
-                padding: [0, 11, 0, 10],
-                align: 'left'
-              },
-              value: {
-                align: 'left',
-                fontSize: 18,
-                lineHeight: 24,
-                color: '#fff',
-                padding: [-6, 0, 0, 0],
-              }
-            }
-          },
-          labelLine: {
-            length: 25,
-            length2: 140
-          },
-          data: this.data
-        }]
-      }
-    }
-  }
-}
+	export default {
+		props: ['data'],
+		watch: {
+			data: {
+				handler(val) {
+					this.$nextTick(() => {
+						this.instance = echarts.init(this.$refs.chart).setOption(this.getOption())
+					})
+				},
+				deep: true,
+				immediate: true,
+			},
+		},
+		methods: {
+			getOption() {
+				const total = this.data.reduce((p, n) => p + n.value, 0)
+				return {
+					title: {
+						text: '区域',
+						subtext: '销售气量',
+						left: 'center',
+						top: '35%',
+						textStyle: {
+							color: '#fff',
+							fontSize: 24,
+							fontWeight: 600
+						},
+						subtextStyle: {
+							color: '#fff',
+							fontSize: 24,
+							fontWeight: 600
+						}
+					},
+					series: [{
+						type: 'pie',
+						center: ['50%', '45%'],
+						radius: ['52%', '64%'],
+						avoidLabelOverlap: true,
+						minAngle: 24,
+						startAngle: 270,
+						color: ['#db4f49', '#367cf6', '#84e4fc', '#f8cd47', '#fceb50', '#ed9144', '#5fcbb9'],
+						label: {
+							formatter: function (params) {
+								const percent = (params.value * 100 / total).toFixed(2)
+								return `{normal|${params.name}}\n{value|${Math.ceil(params.value / 10000)} ${percent}%}`
+							},
+							padding: [0, -100],
+							rich: {
+								normal: {
+									fontSize: 14,
+									lineHeight: 14,
+									color: '#fff',
+									padding: [6, 0, 0, 0],
+									align: 'right'
+								},
+								value: {
+									align: 'left',
+									fontSize: 14,
+									lineHeight: 14,
+									color: '#fff',
+									padding: [-8, 0, 0, 0],
+								}
+							}
+						},
+						labelLine: {
+							length: 25,
+							length2: 110
+						},
+						data: this.data
+					}]
+				}
+			}
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
-  .wrap {
-    position: relative;
-    width: 100%;
-    height: 412px;
-    .unit {
-      position: absolute;
-      bottom: 24px;
-      right: 24px;
-      font-size: 18px;
-      line-height: 24px;
-      color: #FFFFFF;
-    }
-  }
-  .chart {
-    width: 100%;
-    height: 100%;
-    background: url("/static/images/home/loop-bg.svg") no-repeat center;
-  }
+	.wrap {
+		position: relative;
+		width: 100%;
+		height: 412px;
+
+		.unit {
+			position: absolute;
+			bottom: 24px;
+			right: 24px;
+			font-size: 18px;
+			line-height: 24px;
+			color: #FFFFFF;
+		}
+	}
+
+	.chart {
+		width: 100%;
+		height: 100%;
+		background-image: url("/static/images/home/loop-bg.svg");
+		background-repeat: no-repeat;
+		background-position: 50% 40%;
+	}
 </style>
