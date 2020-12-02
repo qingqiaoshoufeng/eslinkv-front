0173aec3b5b5f6b1197b03357742cf82896a4c07
xjm <xujm@citycloud,com.cn>
2020-12-02 09:56:06 +0800

    feat: update

---
+++ src/business-components/Home/h-desc/index.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="widget-part" :style="styles" v-if="data">
+    <div class="title">{{ config.config.title }}</div>
+    <div class="num font-num">{{ data.count | toThousand }}</div>
+	</div>
+</template>
+<script>
+import mixins from '../../mixins';
+import {getInput} from "@lib/views/core/widgets/parts/lib/config-tools";
+
+const config = {
+  animation: true,
+  config: {
+    title: true
+  },
+};
+const value = {
+	api: {
+		data: JSON.stringify({
+      count: 7632935
+    })
+	},
+  config: {
+    title: '年度累计供气量(m³)'
+  }
+}
+const configSource = {
+  config: {
+    fields: {
+      title: getInput('title', '标题')
+    }
+  }
+}
+export default {
+	mixins: [mixins],
+	created() {
+		this.configSource = this.parseConfigSource(config, configSource);
+		this.configValue = this.parseConfigValue(config, value);
+	},
+};
+</script>
+<style lang="scss" scoped>
+.widget-part {
+  .title {
+    height: 32px;
+    font-size: 24px;
+    line-height: 32px;
+    color: #FFFFFF;
+    background: #0057A9;
+    border-right: 6px solid #fff;
+    border-left: 6px solid #fff;
+    margin-bottom: 8px;
+  }
+  .num {
+    background: linear-gradient(180deg, rgba(0, 87, 169, 0.5) 0%, rgba(0, 87, 169, 0) 100%);
+    width: 100%;
+    height: 80px;
+    line-height: 80px;
+    font-weight: bold;
+    font-size: 48px;
+    text-align: center;
+    color: #FFFFFF;
+  }
+}
+</style>
+
