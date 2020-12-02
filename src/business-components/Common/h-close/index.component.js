081f8bdac2b0d48120a4adff17621ebe29c90bc9
xjm <xujm@citycloud,com.cn>
2020-12-02 16:07:22 +0800

    feat: s-bar增加参数

---
+++ src/business-components/Common/h-close/index.component.js
@@ -0,0 +1,13 @@
+export default {
+	config: {
+		layout: {
+			size: {
+				width: 32,
+				height: 32
+			},
+			position: {
+				value: 'relative'
+			}
+		}
+	}
+}
---
+++ src/business-components/Common/h-close/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="widget-part" :style="styles">
+    <img src="./close.svg" @click="back" class="img"/>
+  </div>
+</template>
+<script>
+	import mixins from '../../mixins'
+	import GoldChart from '../../../openApi'
+
+	const config = {animation: true}
+	const value = {
+		api: {
+			data: JSON.stringify({
+				sceneId: ''
+			})
+		}
+	}
+
+	export default {
+		mixins: [mixins],
+		created() {
+			this.configSource = this.parseConfigSource(config);
+			this.configValue = this.parseConfigValue(config, value);
+		},
+		methods: {
+			back() {
+				if (!this.data.sceneId) return
+				GoldChart.scene.destroyScene(this.data.sceneId)
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.img {
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
