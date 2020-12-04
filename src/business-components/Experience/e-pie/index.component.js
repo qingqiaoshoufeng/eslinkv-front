182d38d0e1559858cb033e27a89836411fc2bfc6
xjm <xujm@citycloud,com.cn>
2020-12-04 11:19:56 +0800

    feat: e-pie

---
+++ src/business-components/Experience/e-pie/index.component.js
@@ -0,0 +1,13 @@
+export default {
+	config: {
+		layout: {
+			size: {
+				width: 988,
+				height: 214
+			},
+			position: {
+				value: 'relative'
+			}
+		}
+	}
+}
---
+++ src/business-components/Experience/e-pie/options.js
@@ -0,0 +1,13 @@
+export default {
+	series: [
+		{
+			center: ['10%', '50%'],
+			type: 'pie',
+			radius: ['50%', '62%'],
+			minAngle: 4,
+			label: {
+				show: false,
+			},
+		}
+	]
+}
