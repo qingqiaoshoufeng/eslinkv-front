3e4ba22b89eae78b1fbe1e380dc5d55ede3da753
zhepei.shen <zhepei.shen@eslink.com>
2020-12-05 17:01:58 +0800

    ie

--- vue.config.js
+++ vue.config.js
@@ -83,26 +83,26 @@ module.exports = {
                 threshold: 10240,
                 minRatio: 0.8
             }))
-            config.optimization = {
-                runtimeChunk: 'single',
-                splitChunks: {
-                    chunks: 'all',
-                    maxInitialRequests: Infinity,
-                    minSize: 20000,
-                    cacheGroups: {
-                        vendor: {
-                            test: /[\\/]node_modules[\\/]/,
-                            name(module) {
-                                // get the name. E.g. node_modules/packageName/not/this/part.js
-                                // or node_modules/packageName
-                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-                                // npm package names are URL-safe, but some servers don't like @ symbols
-                                return `npm.${packageName.replace('@', '')}`
-                            }
-                        }
-                    }
-                }
-            }
+            // config.optimization = {
+            //     runtimeChunk: 'single',
+            //     splitChunks: {
+            //         chunks: 'all',
+            //         maxInitialRequests: Infinity,
+            //         minSize: 20000,
+            //         cacheGroups: {
+            //             vendor: {
+            //                 test: /[\\/]node_modules[\\/]/,
+            //                 name(module) {
+            //                     // get the name. E.g. node_modules/packageName/not/this/part.js
+            //                     // or node_modules/packageName
+            //                     const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+            //                     // npm package names are URL-safe, but some servers don't like @ symbols
+            //                     return `npm.${packageName.replace('@', '')}`
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
         }
 		config.entry.app = ["babel-polyfill", "./src/main.ts"];
         config.resolve.extensions = [".js", ".vue", ".json", ".ts", ".tsx"]
