0aaf77720e0d3c93eb3085d4745d1b2e24a8c562
suyan <15757124871@163.com>
2020-12-14 18:45:09 +0800

    gis线粗细修改

--- src/components-map/AMap/a-map/utils/getHangZhouGasGISPosition.js
+++ src/components-map/AMap/a-map/utils/getHangZhouGasGISPosition.js
@@ -1,7 +1,7 @@
 import CoordinateTransform from './CoordinateTransform'
 import TransformClassSlippy from './TransformClassSlippy'
 
-const TILE_WIDTH = 512
+const TILE_WIDTH = 256
 
 const XB = 119.3414085
 const XK = 96366.23457
