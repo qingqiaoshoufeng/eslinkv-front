3995408f0a911162972c5e67fd57977da281626c
xjm <xujm@citycloud,com.cn>
2020-12-16 11:26:11 +0800

    feat: 抢修优化

--- src/business-components/More/h-project1/step-6.vue
+++ src/business-components/More/h-project1/step-6.vue
@@ -3,7 +3,7 @@
 		<div class="h-project-1-step-icon fn-flex">
 			<img :src="icon6" alt=""/>
 		</div>
-		<h2 class="pos-a text-center">等待抢修</h2>
+		<h2 class="pos-a text-center">抢修中</h2>
 	</div>
 </template>
 <script>
