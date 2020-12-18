1ed702b4029dc3de00ea8b17a2d18d05c54e42ac
xjm <xujm@citycloud,com.cn>
2020-12-18 13:13:21 +0800

    feat: 直播优化

--- src/business-components/More/h-project1/h-swiper.vue
+++ src/business-components/More/h-project1/h-swiper.vue
@@ -4,10 +4,25 @@
 		<div class="swiper-Part">
 			<!-- 视频区域 -->
 			<div class="left pos-r">
-        <live-video></live-video>
+        <live-video @getPuList="getPuList" ref="live"></live-video>
 			</div>
 			<!-- 轮播图区域 -->
 			<div class="right">
+        <div class="select" v-if="deviceList.length">
+          <div class="current" @click="isShowDevice = !isShowDevice">
+            <span>{{ deviceList[deviceIndex].Name }}</span>
+            <img src="./img/arrow-down.svg">
+          </div>
+          <div class="options" v-show="isShowDevice">
+            <div
+                class="option"
+                v-for="(k, i) in deviceList"
+                :key="i"
+                :class="{active: i === deviceIndex}"
+                @click="chooseDevice(i)"
+            >{{ k.Name }}</div>
+          </div>
+        </div>
 				<div class="swiper-area">
 					<div class="swiper-container gallery-thumbs swiper-swiper">
 						<div class="swiper-wrapper" ref="backTop">
@@ -49,12 +64,24 @@
 		},
 		data() {
 			return {
+        deviceIndex: 0,
+        deviceList: [],
 				swiper: null,
+        isShowDevice: false,
 				galleryThumbs: null,
 				galleryThumbs1: null,
 			};
 		},
 		methods: {
+	    chooseDevice (index) {
+	      if (index === this.deviceIndex) return
+	      this.deviceIndex = index
+        this.isShowDevice = false
+        this.$refs.live.updateDevice(index)
+      },
+      getPuList (list) {
+        this.deviceList = list
+      },
 			swiperInit(swiper, tag, spaceBetween, slidesPerView, isControl) {
 				let _this = this;
 				let options = {
@@ -134,6 +161,50 @@
 			.right {
 				width: 340px;
 				height: 400px;
+        position: relative;
+
+        .select {
+          position: absolute;
+          top: -42px;
+          right: 10px;
+          .current {
+            width: 180px;
+            height: 32px;
+            display: flex;
+            color: #fff;
+            align-items: center;
+            justify-content: center;
+            background: #0057A9;
+            border: 1px solid #00DDFF;
+            border-radius: 4px;
+            font-weight: 600;
+            font-size: 20px;
+          }
+          .options {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 36px;
+            background: #0057A9;
+            border-radius: 4px;
+            padding: 8px 0;
+            z-index: 9;
+            .option {
+              color: rgba(255, 255, 255, 0.75);
+              text-align: center;
+              font-size: 20px;
+              cursor: pointer;
+              &~.option {
+                margin-top: 8px;
+              }
+              &.active, &:hover {
+                font-weight: 600;
+                color: #fff;
+                background: rgba(0, 221, 255, 0.3);
+              }
+            }
+          }
+        }
 
 				.swiper-area {
 					width: 340px;
