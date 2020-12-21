c36dcba39b1877ed71d87b6bbc722b5198648a34
xjm <xujm@citycloud,com.cn>
2020-12-21 16:31:04 +0800

    s-bar6

--- src/business-components/Serves/s-bar6/index.vue
+++ src/business-components/Serves/s-bar6/index.vue
@@ -1,6 +1,20 @@
 <template>
 	<div class="widget-part" :style="styles" v-if="data">
 		<div class="chart" :id="id"/>
+    <div class="legend">
+      <div class="legend-item">
+        <div class="color border" :style="{borderColor: config.config.color1}"></div>
+        <div class="label">{{ config.config.desc1 }}</div>
+      </div>
+      <div class="legend-item">
+        <div class="color" :style="{backgroundColor: config.config.color2}"></div>
+        <div class="label">{{ config.config.desc2 }}</div>
+      </div>
+      <div class="legend-item">
+        <div class="color" :style="{backgroundColor: config.config.color3}"></div>
+        <div class="label">{{ config.config.desc3 }}</div>
+      </div>
+    </div>
 	</div>
 </template>
 <script>
@@ -25,49 +39,49 @@
 			data: JSON.stringify([
 				{
 					x: '05月',
-					a: 30,
+					a: 50,
 					b: 40,
 					c: 10
 				},
 				{
 					x: '06月',
-					a: 15,
+					a: 23,
 					b: 18,
 					c: 5
 				},
 				{
 					x: '07月',
-					a: 35,
+					a: 52,
 					b: 42,
 					c: 10
 				},
 				{
 					x: '08月',
-					a: 10,
+					a: 15,
 					b: 11,
 					c: 4
 				},
 				{
 					x: '08月',
-					a: 25,
+					a: 38,
 					b: 26,
 					c: 12
 				},
 				{
 					x: '10月',
-					a: 28,
+					a: 51,
 					b: 35,
 					c: 16
 				},
 			])
 		},
 		config: {
-			color1: '#2194FF',
+			color1: '#00DDFF',
 			color2: '#00FFCF',
 			color3: '#E5615B',
 			desc1: '申请数量',
 			desc2: '审核通过',
-			desc3: '待审核',
+			desc3: '审核未通过',
 			unit: '单',
 		}
 	}
@@ -117,5 +131,31 @@
 		height: 100%;
 		position: relative;
 	}
+  .legend {
+    position: absolute;
+    top: 4px;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .legend-item {
+      display: flex;
+      align-items: center;
+      margin-left: 16px;
+      .color {
+        width: 16px;
+        height: 8px;
+        &.border {
+          border: 1px solid #ccc;
+        }
+      }
+      .label {
+        font-size: 16px;
+        margin-left: 4px;
+        color: #FFFFFF;
+      }
+    }
+  }
 </style>
 
