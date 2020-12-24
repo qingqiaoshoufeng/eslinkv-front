2433015936f01e9ad1c95271ddf7b526a07a1586
於志强 <zhiqiang.yu@eslink.com>
2020-12-24 10:32:59 +0800

    杭燃码方法添加注释

--- src/components-map/AMap/a-map/utils/latlngtopx.js
+++ src/components-map/AMap/a-map/utils/latlngtopx.js
@@ -1,19 +1,16 @@
 
+// 通过经纬度 获取坐标方法
 export function lnglat2container(pos, map) {
     var lnglat = new AMap.LngLat(pos[0], pos[1]);
     var pixel = map.lngLatToContainer(lnglat);
     return pixel.round()
 }
 
-
-
-
+// 获取相对距离偏移位置获取panBy方法的偏移量
 export function latlngtopx(pos, map) {
-    // 需要的中心点
+    // 杭燃码需要的中心点
     let x1 = map.getSize().width / 5
     let y1 = map.getSize().height / 2
-    pos
-
     let { x: x2, y: y2 } = lnglat2container(pos, map)
     return { x: x1 - x2, y: y1 - y2 }
 }
