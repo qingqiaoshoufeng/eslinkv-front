276ed17d4bbf3c5734f3f529c68920a40116e55b
於志强 <zhiqiang.yu@eslink.com>
2020-12-24 10:26:18 +0800

    杭燃码轮播位置修正完成

--- src/components-map/AMap/a-map/MapContainer/MapPages/Service/serviceHangranCode/index.vue
+++ src/components-map/AMap/a-map/MapContainer/MapPages/Service/serviceHangranCode/index.vue
@@ -107,7 +107,7 @@ componentCommonArr.map(componentName => {
 });
 
 import pageMixin from '../../../../mixins/pageMixin';
-import { lnglat2container } from '../../../../utils/latlngtopx';
+import { lnglat2container, latlngtopx } from '../../../../utils/latlngtopx';
 import {
 	SERVICE_SERVICEHANGRANCODE_LEGEND_MAP,
 	SERVICE_SERVICEHANGRANCODE_OVERLAY_MAP,
@@ -287,22 +287,6 @@ export default {
 			if (this.intervalId || this.intervalId == 0) {
 				this.clearInterval();
 			}
-			if (!this.intervalId) {
-			}
-			this.allTypeStationList.branchCompanyList.map(item => {
-				let { lat, lng } = item;
-				let { x, y } = lnglat2container(
-					this.center,
-					[lng, lat],
-					this.$amap
-				);
-				item.x = x;
-				item.y = y;
-				console.log(x, y);
-				return { ...item, x: x, y: y };
-			});
-			debugger;
-			console.log(this.allTypeStationList.branchCompanyList);
 			this.intervalId = setInterval(() => {
 				let currentIndex = index++ % length;
 				this.activeOverlay = {
@@ -313,39 +297,11 @@ export default {
 						SERVICE_SERVICEHANGRANCODE_LEGEND_MAP['BranchCompany']
 							.detailList,
 				};
-
-				// let { name } = this.activeOverlay;
-
-				// let address = {
-				// 	杭州天然气有限公司: { value: [121.39105835, 30.49227485] },
-				// 	杭州钱江燃气有限公司: {
-				// 		value: [121.49405518, 30.39419946],
-				// 	},
-				// 	桐庐杭燃燃气有限公司: {
-				// 		value: [120.87769775, 29.85007093],
-				// 	},
-				// 	海宁星港燃气有限公司: {
-				// 		value: [121.56159668, 30.43559623],
-				// 	},
-				// };
-
-				// let address = {
-				// 	杭州天然气有限公司: { value: [121.39105835, 30.49227485] },
-				// 	杭州钱江燃气有限公司: {
-				// 		value: [121.49405518, 30.39419946],
-				// 	},
-				// 	桐庐杭燃燃气有限公司: {
-				// 		value: [120.87769775, 29.85007093],
-				// 	},
-				// 	海宁星港燃气有限公司: {
-				// 		value: [121.56159668, 30.43559623],
-				// 	},
-				// };
 				this.detailInfo = this.activeOverlay.gasCodeMapDetailInfoVO;
+				let { lat, lng } = this.activeOverlay;
+				let { x, y } = latlngtopx([lng, lat], this.$amap);
 				this.showOverlayDetail = true;
-				// let a = 69;
-				console.log(this.activeOverlay);
-				this.$amap.panBy(this.activeOverlay.x, this.activeOverlay.y);
+				this.$amap.panBy(x, y);
 			}, 5000);
 		},
 		// 关闭定时器
--- src/components-map/AMap/a-map/utils/latlngtopx.js
+++ src/components-map/AMap/a-map/utils/latlngtopx.js
@@ -1,6 +1,6 @@
 
-export function lnglat2container1(inputVal, map) {
-    var lnglat = new AMap.LngLat(inputVal[0], inputVal[1]);
+export function lnglat2container(pos, map) {
+    var lnglat = new AMap.LngLat(pos[0], pos[1]);
     var pixel = map.lngLatToContainer(lnglat);
     return pixel.round()
 }
@@ -8,15 +8,12 @@ export function lnglat2container1(inputVal, map) {
 
 
 
-export function lnglat2container(inputVal, inputVal2, map) {
+export function latlngtopx(pos, map) {
     // 需要的中心点
     let x1 = map.getSize().width / 5
     let y1 = map.getSize().height / 2
-    console.log(x1, y1, '余志强')
+    pos
 
-    let { x, y } = lnglat2container1(inputVal, map)
-    let { x: x2, y: y2 } = lnglat2container1(inputVal2, map)
-    y1 = y
-    console.log(x1, y1, x, y, x2, y2, map)
-    return { x: x + x1 - x2, y: y + y1 - y2 }
+    let { x: x2, y: y2 } = lnglat2container(pos, map)
+    return { x: x1 - x2, y: y1 - y2 }
 }
