38edfa82a1dd2d21ac8b4490c2401c569d3e35b5
yan.su <yan.su@eslink.com>
2020-12-25 11:15:05 +0800

    调试bug

--- src/components-map/AMap/a-map/lib/components/amap-marker.vue
+++ src/components-map/AMap/a-map/lib/components/amap-marker.vue
@@ -91,6 +91,7 @@ export default {
 					flag === false ? this.hide() : this.show();
 				},
 				position(lnglat) {
+					console.log(lnglat, 'lnglat');
 					this.setPosition(new AMap.LngLat(...lnglat));
 				},
 			},
