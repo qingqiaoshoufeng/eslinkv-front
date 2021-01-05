7b12357ab4beffe1102087d77e2f70e5fc087aa6
zhepei.shen <zhepei.shen@eslink.com>
2021-01-05 19:36:26 +0800

    合并代码

--- src/components-market/index.ts
+++ src/components-market/index.ts
@@ -1,15 +1,15 @@
-// import {getCompList, getLevel0} from '@/api/bussiness.api'
-//
-// let widgetsArray: Array<any> = []
-//
-// getLevel0().then(res => {
-// 	res.forEach(child => {
-// 		getCompList({componentTypeId: child.componentTypeId}).then(res => {
-// 			widgetsArray = res
-// 			window.GoldChart.mutations.setCustomWidgets({
-// 				label: child.componentTypeName,
-// 				widgets: widgetsArray
-// 			})
-// 		})
-// 	})
-// })
+import {getCompList, getLevel0} from '@/api/bussiness.api'
+
+let widgetsArray: Array<any> = []
+
+getLevel0().then(res => {
+	res.forEach(child => {
+		getCompList({componentTypeId: child.componentTypeId}).then(res => {
+			widgetsArray = res
+			window.GoldChart.mutations.setCustomWidgets({
+				label: child.componentTypeName,
+				widgets: widgetsArray
+			})
+		})
+	})
+})
