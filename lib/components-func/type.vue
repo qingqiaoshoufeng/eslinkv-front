63e63995940bc17ee987474aec86d9d3cd1f7ba5
zhepei.shen <zhepei.shen@eslink.com>
2021-01-22 15:42:19 +0800

    refactor: id,type 添加

---
+++ lib/components-func/type.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+	.d-manage-modal-control
+		label 组件类型
+		Input(v-model="item.type" :disabled="true")
+	</div>
+</template>
+<script lang="ts">
+	import func from './mixins'
+	import {mixins} from 'vue-class-component'
+	import {Component} from 'vue-property-decorator'
+	
+	@Component
+	export default class extends mixins(func) {
+		
+	}
+</script>
