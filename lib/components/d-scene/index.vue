734b1e3beca06b3ccda58387ec8b9556f95796bd
zhepei.shen <zhepei.shen@eslink.com>
2021-01-25 13:02:09 +0800

    refactor: 改造

--- lib/components/d-scene/index.vue
+++ lib/components/d-scene/index.vue
@@ -1,108 +1,66 @@
 <template lang="pug">
-	.d-scene-box.pos-a
-		.d-scene.pos-a.pointer
-			Icon(type="logo-buffer" title="场景切换" @click="handleClick" size="28" :class="[{active:sceneModal}]")
-		.d-scene-modal.pos-a(:class="[{active:sceneModal}]")
-			header
-				span 场景切换
-				span 当前：场景{{store.scene.index}}
-			ul
-				li.pointer.fn-flex.pos-r.flex-row(@click="changeScene(0)" :class="[{active:store.scene.index===0}]")
-					span 主场景
-					p ID：0
-				li.pointer.fn-flex.pos-r.flex-row(v-for="(item,index) in store.scene.list" :class="[{active:store.scene.index===item}]" @click="changeScene(item)" :key="item")
-					input(@input="e=>handleSceneName(item,e)" :value="store.scene.obj[item].name")
-					p ID：{{item}}
-					Icon.destroy(type="ios-trash-outline" @click="e=>destroyScene(e,index)")
+	d-right-modal.d-scene-modal(title="场景切换" ref="modal" :width="360" icon="logo-buffer" :top="105")
+		header
+			span 场景切换
+			span 当前：场景{{store.scene.index}}
+		ul
+			li.pointer.fn-flex.pos-r.flex-row(@click="changeScene(0)" :class="[{active:store.scene.index===0}]")
+				span 主场景
+				p ID：0
+			li.pointer.fn-flex.pos-r.flex-row(v-for="(item,index) in store.scene.list" :class="[{active:store.scene.index===item}]" @click="changeScene(item)" :key="item")
+				input(@input="e=>handleSceneName(item,e)" :value="store.scene.obj[item].name")
+				p ID：{{item}}
+				Icon.destroy(type="ios-trash-outline" @click="e=>destroyScene(e,index)")
+			li.pointer.create.fn-flex(@click="createScene") +
 </template>
-<script>
+<script lang="ts">
+	import dRightModal from '../d-right-modal'
+	import {Component, Vue} from 'vue-property-decorator'
 	import {Icon} from 'view-design'
 
-	export default {
+	@Component({
 		components: {
-			Icon
+			dRightModal, Icon
 		},
-		data() {
-			return {
-				store: window.GoldChart.store,
-				sceneModal: false
-			}
-		},
-		methods: {
-			handleSceneName(key, e) {
-				window.GoldChart.mutations.setSceneName(key, e.target.value)
-			},
-			changeScene(index) {
-				window.GoldChart.mutations.setSceneIndex(index)
-				this.store.events.list.widgetUnActived()
-			},
-			handleClick() {
-				this.sceneModal = !this.sceneModal
-				this.$emit('show')
-			},
-			createScene() {
-				window.GoldChart.mutations.createScene()
-			},
-			destroyScene(e, index) {
-				e.stopPropagation()
-				this.$Modal.confirm({
-					title: '提示',
-					content: '是否删除当前场景？',
-					onOk: () => {
-						if (window.GoldChart.store.scene.index === window.GoldChart.store.scene.list[index]) {
-							window.GoldChart.mutations.setSceneIndex(window.GoldChart.store.scene.list[0])
-						}
-						window.GoldChart.mutations.deleteScene(index)
+	})
+	export default class extends Vue {
+		store: any = window.GoldChart.store
+		sceneModal: boolean = false
+
+		handleSceneName(key, e) {
+			window.GoldChart.mutations.setSceneName(key, e.target.value)
+		}
+
+		changeScene(index) {
+			window.GoldChart.mutations.setSceneIndex(index)
+			this.store.events.list.widgetUnActived()
+		}
+
+		handleClose() {
+			this.$refs.modal.rightModal = false
+		}
+
+		createScene() {
+			window.GoldChart.mutations.createScene()
+		}
+
+		destroyScene(e, index) {
+			e.stopPropagation()
+			this.$Modal.confirm({
+				title: '提示',
+				content: '是否删除当前场景？',
+				onOk: () => {
+					if (window.GoldChart.store.scene.index === window.GoldChart.store.scene.list[index]) {
+						window.GoldChart.mutations.setSceneIndex(window.GoldChart.store.scene.list[0])
 					}
-				})
-			}
+					window.GoldChart.mutations.deleteScene(index)
+				}
+			})
 		}
 	}
 </script>
 <style lang="scss" scoped>
-	.d-scene-box {
-		width: 24px;
-		height: 24px;
-		right: 15px;
-		top: 105px;
-	}
-
-	.d-scene {
-		right: 0;
-		top: 0;
-		z-index: 2;
-		width: 24px;
-		height: 24px;
-
-		.ivu-icon {
-			opacity: .6;
-
-			&:hover, &.active {
-				opacity: 1;
-			}
-		}
-	}
-
 	.d-scene-modal {
-		right: -380px;
-		top: -70px;
-		width: 360px;
-		background-color: white;
-		border-radius: 5px;
-		overflow: hidden;
-		z-index: 3;
-		letter-spacing: 0;
-		height: calc(100vh - 140px);
-		transition: all .3s;
-		visibility: hidden;
-		opacity: 0;
-
-		&.active {
-			visibility: inherit;
-			right: 40px;
-			opacity: 1;
-		}
-
 		ul {
 			height: calc(100% - 42px);
 			overflow-y: auto;
