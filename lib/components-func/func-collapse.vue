8856d112eb5c3f6ecbf7c051e4334df8a230492e
xjm <jiaming.xu@eslink.com>
2021-01-28 18:55:33 +0800

    fix: 折叠

---
+++ lib/components-func/func-collapse.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+	.d-manage-modal-control
+		i-collapse(simple)
+			i-panel {{config.label}}
+				div(slot="content")
+					DManageItem(:config="k" v-for="(k, i) in config.props" :key="i")
+</template>
+<script lang="ts">
+import func from './mixins'
+import {mixins} from 'vue-class-component'
+import {Component} from 'vue-property-decorator'
+import DManageItem from '../components/d-manage/item'
+
+@Component({components: { DManageItem }})
+export default class FuncCollapse extends mixins(func) {
+
+}
+</script>
