29122b958712675af6c8b539b2f05b2cadfb1786
zhepei.shen <zhepei.shen@eslink.com>
2021-02-03 09:54:37 +0800

    feat: footer

--- lib/components/d-footer/index.vue
+++ lib/components/d-footer/index.vue
@@ -18,6 +18,11 @@
 	import {Vue, Component} from 'vue-property-decorator'
 	import {Icon, Button} from 'view-design'
 	import platform from '../../store/platform.store'
+	import scene from '../../store/scene.store'
+	import commonConfigValue from '../../views/core/widgets/parts/lib/common-config-value'
+	import copy from 'fast-copy'
+	import format from 'date-fns/format'
+	import {isObjectString} from '../../utils'
 
 	@Component({
 		components: {'i-icon': Icon},
@@ -25,17 +30,93 @@
 	})
 	export default class DFooter extends Vue {
 		platform = platform.state
+		scene = scene.state
 
 		handleClick() {
 			window.open(`${location.origin}${location.pathname}#/help/HowToUseMarket`)
 		}
 
+		getAttr(o, str) {
+			const arr = str.split('.')
+			let res = o
+			arr.forEach(v => {
+				res = res[v]
+			})
+			return res
+		}
+
+		checkAttr(o, str = '', defaultConfig) {
+			for (const key in o) {
+				const prop = str ? str + '.' + key : key
+				if (Array.isArray(o[key]) && o[key].length > 0) {
+					if (JSON.stringify(o[key]) === JSON.stringify(this.getAttr(defaultConfig, prop))) {
+						o[key] = 'default'
+					}
+				} else if (Object.prototype.toString.call(o[key]) === '[object Object]') {
+					if (JSON.stringify(o[key]) === JSON.stringify(this.getAttr(defaultConfig, prop))) {
+						o[key] = 'default'
+					} else {
+						this.checkAttr(o[key], prop, defaultConfig)
+					}
+				}
+			}
+		}
+
+		platFormData() {
+			const defaultConfig = commonConfigValue() // 读取默认配置
+			const panelConfig = this.platform.panelConfig
+			const {size, info: {name, remark}} = panelConfig
+			delete size.preset
+			if (size.range && !Object.values(size.range).find(item => item !== 0 && item !== '%')) {
+				delete size.range
+			}
+			const widgetAdded = copy(this.platform.widgetAdded)
+			const widgets = Object.values(widgetAdded).map(({id, market = false, type, config, scene = 0}) => {
+				const api = config.api
+				if (api && api.data) {
+					if (isObjectString(api.data)) {
+						try {
+							api.data = JSON.stringify(JSON.parse(api.data))
+						} catch (e) {
+							throw new Error(e)
+						}
+					}
+				}
+				this.checkAttr(config, '', defaultConfig)
+				return {
+					id,
+					scene,
+					type,
+					market,
+					value: {...config}
+				}
+			})
+			
+			// todo 参考线数据放入store，获取store中的数据
+			// const guides = this.guides
+			const guides = []
+			return {
+				name,
+				remark,
+				createTime: format(Date.now(), 'yyyy-MM-dd hh:mm:ss'),
+				attribute: JSON.stringify({
+					kanboard: panelConfig, // 看板画布配置
+					widgets, // 小工具配置
+					apis: [], // 全局数据源配置
+					grids: {}, // 布局格子
+					scene: this.scene.obj, // 场景
+					guides // 参考线
+				})
+			}
+		}
+
 		mounted() {
 			const {params: {id}} = this.$route
+			// todo 通过name/id 判断是否是new/edit/local/full/detail 显示不同的button
 			console.log(id)
 		}
-		
-		
+
+
 	}
 </script>
 <style lang="scss" scoped>
