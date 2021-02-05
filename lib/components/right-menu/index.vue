09d34b1a46de49e7f026b54a43e99e8a64f9e3d0
xjm <jiaming.xu@eslink.com>
2021-02-05 15:10:01 +0800

    fix: 右键菜单优化

--- lib/components/right-menu/index.vue
+++ lib/components/right-menu/index.vue
@@ -5,36 +5,82 @@
 			span.suffix 当前 {{ zIndex }}
 			ul.sub-menu.pos-a
 				li(@click="handleZIndex(1)") 上移一层
-				li( @click="handleZIndex(-1)") 下移一层
+				li(@click="handleZIndex(-1)") 下移一层
 				li(@click="handleZIndexTop") 置顶
-				li( @click="handleZIndexBottom") 置底
+				li(@click="handleZIndexBottom") 置底
 		li(@click="copyWidget") 复制
 		li(@click="handleUnActive") 取消选定
 		li(@click="handleLock") {{ isLock ? '解锁' : '锁定' }}
 		li(@click="hideWidget") 隐藏
 		li(@click="deleteWidget") 删除
-	</ul>
 </template>
 <script lang="ts">
-	import {Vue, Component, Prop} from 'vue-property-decorator'
+	import {Vue, Component, Prop, Watch} from 'vue-property-decorator'
 	import platform from '../../store/platform.store'
-
+	import copy from "fast-copy";
+	import {uuid} from '../../utils'
+	
 	@Component
 	export default class rightMenu extends Vue {
 		platform = platform.state
-		@Prop deleteWidget
-		@Prop copyWidget
+		isLock = false
+		zIndex = 10
+		minZIndex = 0
+		maxZIndex = 0
 
 		handleZIndex(num) {
-			this.platform.widgetAdded[this.platform.chooseWidgetId].config.layout.zIndex = this.zIndex + num
+			if (this.zIndex === 1 && num === -1) return
+			this.zIndex += num 
+			this.platform.widgetAdded[this.platform.chooseWidgetId].config.layout.zIndex = this.zIndex
 		}
 
 		handleZIndexTop() {
-
+			this.zIndex = this.maxZIndex
+			this.platform.widgetAdded[this.platform.chooseWidgetId].config.layout.zIndex = this.maxZIndex
 		}
 
 		handleZIndexBottom() {
+			this.zIndex = this.minZIndex
+			this.platform.widgetAdded[this.platform.chooseWidgetId].config.layout.zIndex = this.minZIndex
+		}
 
+		hideWidget() {
+			const widget = this.platform.widgetAdded[this.platform.chooseWidgetId].config.widget
+			widget.hide = !widget.hide
+			this.$emit('deactivateWidget', this.platform.chooseWidgetId)
+		}
+
+		deleteWidget() {
+			this.$Modal.confirm({
+				title: '提示',
+				content: '是否删除当前组件？',
+				onOk: () => {
+					const id = this.platform.chooseWidgetId
+					platform.actions.unChooseWidget()
+					this.$delete(this.platform.widgetAdded, id)
+				}
+			})
+		}
+
+		copyWidget() {
+			const copyId = this.platform.chooseWidgetId
+			const widget = this.platform.widgetAdded[copyId]
+			if (!widget) return
+			const copiedWidget = copy(widget)
+			const id = uuid()
+			copiedWidget.id = id
+			this.initCopyWidget(id, copiedWidget)
+		}
+
+		initCopyWidget(id, widget, makeOffset = true) {
+			const config = widget.config
+			config.widget.id = id
+			const layout = config.layout
+			if (makeOffset) {
+				layout.position.left += 10
+				layout.position.top += 10
+			}
+			platform.actions.setWidgetsAddedItem(id, widget.type, widget.config, widget.scene)
 		}
 
 		handleUnActive() {
@@ -44,13 +90,22 @@
 		handleLock() {
 			this.platform.widgetAdded[this.platform.chooseWidgetId].config.widget.locked = !this.isLock
 		}
-
-		get isLock() {
-			return this.platform.widgetAdded[this.platform.chooseWidgetId].config.widget.locked
-		}
-
-		get zIndex() {
-			return this.platform.widgetAdded[this.platform.chooseWidgetId].config.layout.zIndex
+		
+		@Watch('platform.chooseWidgetId')
+		chooseIdChange (val) {
+			if (!val) return
+			this.isLock = this.platform.widgetAdded[val].config.widget.locked
+			this.zIndex = this.platform.widgetAdded[val].config.layout.zIndex
+			let max = 0
+			let min = 9999
+			for (const key in this.platform.widgetAdded) {
+				max = Math.max(this.platform.widgetAdded[key].config.layout.zIndex, max)
+				min = Math.min(this.platform.widgetAdded[key].config.layout.zIndex, min)
+			}
+			max++
+			min--
+			this.maxZIndex = max
+			this.minZIndex = Math.max(1, min)
 		}
 	}
 </script>
