5b54c4c6f965a4cded80a641511f42d44f322305
zhepei.shen <zhepei.shen@eslink.com>
2021-02-24 08:31:04 +0800

    feat: 复制场景id

---
+++ src/components-business/Common/h-create/index.component.ts
@@ -0,0 +1,17 @@
+const config = {animation: true}
+const value = {
+	api: {
+		data: JSON.stringify({title: '标题', scene: '0'})
+	},
+	layout: {
+		size: {
+			width: 480,
+			height: 43
+		},
+		position: {
+			value: 'relative'
+		}
+	}
+}
+
+export {config, value}
