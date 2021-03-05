2cf3568c4e69decbf99a5f261de9801520aa7e92
zhepei.shen <zhepei.shen@eslink.com>
2021-03-05 17:46:46 +0800

    fix: 三方组件

--- src/utils/index.js
+++ src/utils/index.js
@@ -49,26 +49,37 @@ export function findComponentsDownward(context, componentName) {
 	}, []);
 }
 
-export function loadJs(src) {
+/**
+ * @description 加载三方包
+ */
+export function loadJs(src, value) {
 	if (typeof src === 'string') {
 		return new Promise(resolve => {
-			const el = document.createElement('script')
-			el.src = src
-			el.onload = () => {
+			if (window[value]) {
 				resolve()
-			}
-			document.head.appendChild(el)
-		})
-	} else {
-		let p = []
-		src.forEach(item => {
-			p.push(new Promise(resolve => {
+			} else {
 				const el = document.createElement('script')
-				el.src = item
+				el.src = src
 				el.onload = () => {
 					resolve()
 				}
 				document.head.appendChild(el)
+			}
+		})
+	} else {
+		let p = []
+		src.forEach((item, index) => {
+			p.push(new Promise(resolve => {
+				if (window[value[index]]) {
+					resolve()
+				} else {
+					const el = document.createElement('script')
+					el.src = item
+					el.onload = () => {
+						resolve()
+					}
+					document.head.appendChild(el)
+				}
 			}))
 		})
 		return Promise.all(p)
