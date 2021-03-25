c6519e176ed54a0248c308402468a61decf65d4a
zhepei.shen <zhepei.shen@eslink.com>
2021-03-25 18:48:26 +0800

    fix: test

--- vue.config.js
+++ vue.config.js
@@ -124,7 +124,7 @@ module.exports = {
 		config.plugins.push(
 			new webpack.DefinePlugin({
 				'process.env.version': JSON.stringify(pkg.version),
-				'process.env.staticVuePath': JSON.stringify(isProduction ? 'vue.min.js' : 'vue.js'),
+				'process.env.staticVuePath': JSON.stringify(isProduction ? 'vue.js' : 'vue.js'),
 				'process.env.BUILD_MODE': JSON.stringify(process.env.BUILD_MODE),
 				'process.env.staticPath': JSON.stringify('')
 			})
