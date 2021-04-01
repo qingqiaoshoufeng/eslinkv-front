852feff477a34bdc8519823a094570fbc99a5143
zhepei.shen <zhepei.shen@eslink.com>
2021-04-01 09:43:26 +0800

    Revert "feat: 暂存"
    This reverts commit b1feb4c5

--- src/components-map/index.ts
+++ src/components-map/index.ts
@@ -1,7 +1,6 @@
 import { custom } from 'eslinkv-npm'
 
-const components = {}
-const snapshots = {}
+const components = {}; const snapshots = {}
 const widgetsObject = []
 const w = {}
 const conf = require.context('./', true, /\.(component.js)$/)
