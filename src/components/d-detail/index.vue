b6638dd69f69d0151a2d8dbfdc14b875f07c40d6
zhepei.shen <zhepei.shen@eslink.com>
2021-05-13 18:42:38 +0800

    style: 添加提示按钮

--- src/components/d-detail/index.vue
+++ src/components/d-detail/index.vue
@@ -4,24 +4,26 @@
 		.d-detail-left-icon.fn-flex
 			i-icon.pointer(type="ios-arrow-dropleft", @click="exit", :size="24")
 		.d-detail-left-icon-box.fn-flex
-			i-icon.pointer(
-				title="组件",
-				type="ios-cube-outline",
-				@click="handleLeft1",
-				:size="18",
-				:class="{ active: left1 }")
-			i-icon.pointer(
-				title="场景",
-				type="ios-photos-outline",
-				@click="handleLeft2",
-				:size="18",
-				:class="{ active: left2 }")
-			i-icon.pointer(
-				title="设置",
-				type="ios-archive-outline",
-				@click="handleRight1",
-				:size="18",
-				:class="{ active: right1 }")
+			i-tooltip(placement="bottom", content="组件区")
+				i-icon.pointer(
+					type="ios-cube-outline",
+					@click="handleLeft1",
+					:size="18",
+					:class="{ active: left1 }")
+			i-tooltip(placement="bottom", content="场景区")
+				i-icon.pointer(
+					title="场景",
+					type="ios-photos-outline",
+					@click="handleLeft2",
+					:size="18",
+					:class="{ active: left2 }")
+			i-tooltip(placement="bottom", content="设置区")
+				i-icon.pointer(
+					title="",
+					type="ios-archive-outline",
+					@click="handleRight1",
+					:size="18",
+					:class="{ active: right1 }")
 	.d-detail-middle.fn-flex
 		span.d-detail-title {{ platform.screenName }}
 	ul.d-detail-right.fn-flex
@@ -56,7 +58,15 @@
 </template>
 <script lang="ts">
 import { Component, Prop } from 'vue-property-decorator'
-import { Icon, Button, Modal, Form, FormItem, Input } from 'view-design'
+import {
+	Icon,
+	Button,
+	Modal,
+	Form,
+	FormItem,
+	Input,
+	Tooltip,
+} from 'view-design'
 import copy from 'fast-copy'
 import { mixins } from 'vue-class-component'
 const { commonConfigValue } = eslinkV
@@ -78,6 +88,7 @@ import dShareDialog from '../d-share-dialog/index.vue'
 		'i-form': Form,
 		'i-form-item': FormItem,
 		'i-input': Input,
+		'i-tooltip': Tooltip,
 	},
 })
 export default class DDetail extends mixins(
