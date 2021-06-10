34c22f95123f4f0f6acbb6930ef2c8cbbc42f80b
zhepei.shen <zhepei.shen@eslink.com>
2021-06-10 14:20:20 +0800

    fix: a-map

--- src/components-map/index.ts
+++ src/components-map/index.ts
@@ -1,19 +1,11 @@
 import { Editor } from '@eslinkv/core'
 const editor: Editor = Editor.Instance()
-const component = require.context(
-	'../components-business',
-	true,
-	/index\.(vue)$/,
-)
+const component = require.context('../components-map', true, /index\.(vue)$/)
 const avatar = require.context(
-	'../components-business',
+	'../components-map',
 	true,
 	/snapshot\.(jpg|png)$/,
 )
-const conf = require.context(
-	'../components-business',
-	true,
-	/\.(component.ts)$/,
-)
+const conf = require.context('../components-map', true, /\.(component.ts)$/)
 
 editor.localInit({ name: '杭图', component, avatar, conf })
