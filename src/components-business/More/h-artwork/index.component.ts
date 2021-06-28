5b528fbbba03af7f332689d457e098e192857660
fish-uncle <fishuncle@126.com>
2021-06-28 15:39:22 +0800

    feat: publish

---
+++ src/components-business/More/h-artwork/index.component.ts
@@ -0,0 +1,16 @@
+export const value = {
+	api: {
+		data: JSON.stringify({
+			image: '北门站',
+		}),
+	},
+	layout: {
+		size: {
+			width: 1888,
+			height: 605,
+		},
+		position: {
+			value: 'relative',
+		},
+	},
+}
