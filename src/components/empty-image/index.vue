fe1880e658eef20edd2aa8f12dfb8b7f12b5ab16
zhepei.shen <zhepei.shen@eslink.com>
2021-08-05 13:05:39 +0800

    feat: 添加大屏默认图

--- src/components/empty-image/index.vue
+++ src/components/empty-image/index.vue
@@ -4,29 +4,36 @@ div(
 	:style="{ backgroundImage: `url(${image})`, backgroundPosition: 'center', backgroundSize, backgroundRepeat: 'no-repeat' }",
 	@click="handleClick")
 	slot
-.empty-image(v-else, @click="handleClick")
+.empty-image(
+	v-else,
+	@click="handleClick",
+	:style="{ backgroundImage: `url(${empty[index]})` }")
 	slot
 </template>
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 
+const empty = [
+	require('../../assets/empty/screen-empty1.jpeg'),
+	require('../../assets/empty/screen-empty2.jpeg'),
+	require('../../assets/empty/screen-empty3.jpeg'),
+	require('../../assets/empty/screen-empty4.jpeg'),
+]
 @Component
 export default class EmptyImage extends Vue {
 	@Prop(String) image: string
 	@Prop({ default: '100% 100%' }) backgroundSize: string
-
-	handleClick() {
+	index = Math.floor(Math.random() * 4)
+	empty = empty
+	handleClick(): void {
 		this.$emit('click')
 	}
 }
 </script>
 <style lang="scss">
 .empty-image {
-	background-color: #ddd;
-	background-image: url('/static/images/logo.png');
-	filter: grayscale(100%);
 	background-repeat: no-repeat;
 	background-position: center;
-	background-size: 20% !important;
+	background-size: 100%;
 }
 </style>
